275125fb9385240a7622f25093a68cb4bb923eec
mendiadi <adim333@gmail.com>
2022-08-03 21:42:47 +0300

    polish and add some comments

--- API/api_user.py
+++ API/api_user.py
@@ -3,20 +3,39 @@ from API.baseApi import BaseApi
 
 
 class ApiUser(BaseApi):
+    """
+    Class to make the request calls to account
+    """
 
     def __init__(self, url: str):
         super().__init__(url)
         self._url = f"{self._base_url}/user"
 
     def get_user_login(self, username: str, password: str) -> (int, str):
+        """
+        request get method with username and password
+        should login if account exists
+        :param username: username of the account (string)
+        :param password: password of the account (string)
+        :return: response status code and response text
+        """
         response = self.session.get(f"{self._url}/login?username={username}&password={password}")
         return response.status_code, response.text
 
     def get_user_logout(self) -> (int, str):
+        """
+        request get method to logout account that already logged in
+        :return: response status code and response text
+        """
         response = self.session.get(f"{self._url}/logout")
         return response.status_code, response.text
 
     def post_create_user(self, data: dict) -> (int,str):
+        """
+        make post request and create new user
+        :param data: new data to create user
+        :return:  response status code and response text
+        """
         response = self.session.post(f"{self._url}", json=data)
         return response.status_code, response.text
 
